3d96f94e36b3e34c493e5d9ae83e6a326e86b7aa
Федя Неделькин <fffedosss898@gmail.com>
2024-12-08 22:27:32 +0500

    фыв

--- Tyuiu.NedelkinFA.Sprint5.Task0.V10.Lib/DataService.cs
+++ Tyuiu.NedelkinFA.Sprint5.Task0.V10.Lib/DataService.cs
@@ -5,16 +5,16 @@ namespace Tyuiu.NedelkinFA.Sprint5.Task0.V10.Lib
 {
     public class DataService : ISprint5Task0V10
     {
-        public double CalculateFunction(int x)
+        public string CalculateFunction(int x)
         {
-            return 1.6 * Math.Pow(x, 3) - 2.1 * Math.Pow(x, 2) + 7 * x;
+            double result = 1.6 * Math.Pow(x, 3) - 2.1 * Math.Pow(x, 2) + 7 * x;
+            return Math.Round(result, 3).ToString("F3").Replace('.', ','); 
         }
 
         public string SaveToFileTextData(int x)
         {
-            double result = CalculateFunction(x);
+            string formattedResult = CalculateFunction(x);
             string path = Path.Combine(Path.GetTempPath(), "OutPutFileTask0.txt");
-            string formattedResult = result.ToString("F3").Replace('.', ',');
             File.WriteAllText(path, $"\"{formattedResult}\"");
             return path;
         }
--- Tyuiu.NedelkinFA.Sprint5.Task0.V10/Program.cs
+++ Tyuiu.NedelkinFA.Sprint5.Task0.V10/Program.cs
@@ -3,8 +3,8 @@ using Tyuiu.NedelkinFA.Sprint5.Task0.V10.Lib;
 
 int x = 2;
 DataService ds = new DataService();
-double result = ds.CalculateFunction(x);
-Console.WriteLine("F(" + x + ") = " + result.ToString("F3").Replace('.', ','));
+string result = ds.CalculateFunction(x);
+Console.WriteLine("F(" + x + ") = " + result);
 string filePath = ds.SaveToFileTextData(x);
 Console.WriteLine("file: " + filePath);
 Console.WriteLine("создан!");
