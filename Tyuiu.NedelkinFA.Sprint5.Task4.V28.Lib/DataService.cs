4a19736470357676e7e134ec92ea3de8bf6575ba
Федя Неделькин <fffedosss898@gmail.com>
2024-12-11 00:13:21 +0500

    пваппвавпапав

--- Tyuiu.NedelkinFA.Sprint5.Task4.V28.Lib/DataService.cs
+++ Tyuiu.NedelkinFA.Sprint5.Task4.V28.Lib/DataService.cs
@@ -7,7 +7,7 @@ namespace Tyuiu.NedelkinFA.Sprint5.Task4.V28.Lib
     {
         public double LoadFromDataFile(string path)
         {
-            string strX = File.ReadAllText(path);
+            string strX = File.ReadAllText(path).Trim();
             double x = double.Parse(strX.Replace(',', '.'));
             double result = Math.Round((3 * x * x * x) / Math.Sin(x), 3);
             return result;
